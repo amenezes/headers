9ff67c7c991076a3c30b86d98d6a9ce5536ad6ec
alexandre menezes <alexandre.fmenezes@gmail.com>
2017-03-19 16:41:07 -0300

    version 3.x fix

---
+++ secureheaders/lib/__about__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyleft__",
+]
+
+__title__ = "SecureHeaders"
+__summary__ = ("An application to catch, search and analyze HTTP secure headers.")
+__uri__ = "http://github.com/riramar/headers / http://github.com/amenezes"
+
+__version__ = "3.1.0"
+
+__author__ = "The SecureHeaders team"
+__email__ = "alexandre.fmenezes@owasp.org | ricardo.iramar@owasp.org"
+
+__license__ = "GNU License, Version 2.0"
+__copyleft__ = "Copyleft 2017 {0}".format(__author__)
